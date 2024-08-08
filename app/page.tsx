adc59085d6bd3f006a296ee4fc43c71f95bac808
Jakub <jakuboleszczuk@protonmail.com>
2024-08-08 04:23:15 +0100

    small updates experimenting

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>mvxd.net WIP</h1>
+    <main className="flex min-h-20 flex-col items-center justify-between p-24">
+        <h1 className="text-5xl">mvxd.net WIP</h1>
+        <article className="p-10 text-lg">
+          Hello, I'm Jakub I'm an IT student (yet to be in October), I'm interested in IT programming, I enjoy studying foreign languages.
+        </article>
+        <footer>
+          Github: @scmisa
+        </footer>
     </main>
   );
 }
