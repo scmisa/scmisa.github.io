a6d2f56531c1958a66984685351c07c6cf36b983
Jakub <jakuboleszczuk@protonmail.com>
2024-09-04 22:07:06 +0200

    Applied fontello icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import "./css/fontello.css";
 
 export default function Home() {
   return (
     <main className="flex min-h-20 flex-col items-center justify-between p-24">
         <h1 className="text-5xl">mvxd.net WIP</h1>
-        <article className="p-10 text-lg text-justify ">
+        <article className="p-10 text-lg text-justify">
           Hello, I'm Jakub I'm an IT student (yet to be in October), <br></br>
           I'm interested in IT programming, I enjoy studying foreign languages.<br></br>
           I'm currently learning Japanese and Korean. <br></br>
@@ -15,7 +16,9 @@ export default function Home() {
           which took place in Warsaw H.4.0.S gamebar.
         </article>
         <footer>
+          <i className="demo-icon icon-github-circled"></i>
           Github: <a href="https://github.com/scmisa">@scmisa</a><br></br>
+          <i className="demo-icon icon-linkedin-squared"></i>
           LinkedIn: <a href="https://www.linkedin.com/in/jakub-oleszczuk/">Jakub Oleszczuk</a>
         </footer>
     </main>
