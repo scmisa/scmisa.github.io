52b295ab3a26e144e50cffaa338f1f8c8ed5cef4
Jakub <jakuboleszczuk@protonmail.com>
2024-09-04 19:37:00 +0200

    Update page.tsx file with new content

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,14 @@ export default function Home() {
         <h1 className="text-5xl">mvxd.net WIP</h1>
         <article className="p-10 text-lg">
           Hello, I'm Jakub I'm an IT student (yet to be in October), I'm interested in IT programming, I enjoy studying foreign languages.
+          I'm currently learning Japanese and Korean. I'm also interested in the culture of these countries.
+          I'm also part of the StarCraft 2 community, I'm a fan of the game and I'm interested in the competitive scene.
+          I recetly took part of research project about StarCraft 2, where I was responsible for ECG Sensors recording and data analysis,<br></br>
+          which took place in Warsaw H.4.0.S gamebar.
         </article>
         <footer>
-          Github: @scmisa
+          Github: <a href="https://github.com/scmisa">@scmisa</a>
+          LinkedIn: <a href="https://www.linkedin.com/in/jakub-oleszczuk/">Jakub Oleszczuk</a>
         </footer>
     </main>
   );
